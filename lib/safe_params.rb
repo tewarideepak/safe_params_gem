2439ae2759fe1c9bc22319389b5ae7fe31289ff5
Deepak Tewari <tewarid69@gmail.com>
2025-07-29 22:59:33 +0530

    Add ActiveSupport::Concern and auto-include SafeParams in ActiveRecord models for proper class method support

--- lib/safe_params.rb
+++ lib/safe_params.rb
@@ -1,3 +1,4 @@
+
 require "safe_params/version"
 require "safe_params/railtie" if defined?(Rails)
 require "safe_params/helper"
@@ -5,19 +6,27 @@ require "safe_params/config_loader"
 require "safe_params/cli"
 
 module SafeParams
-  def self.included(base)
-    base.extend ClassMethods
+  extend ActiveSupport::Concern
+
+  included do
+    class_attribute :_safe_attrs, instance_writer: false, default: []
+    class_attribute :_nested_attrs, instance_writer: false, default: {}
   end
 
-  module ClassMethods
+  class_methods do
     def safe_params(*attrs, nested: {})
-      @safe_attrs = attrs.map(&:to_sym)
-      @nested_attrs = nested.transform_keys(&:to_sym)
+      self._safe_attrs = attrs.map(&:to_sym)
+      self._nested_attrs = nested.transform_keys(&:to_sym)
     end
 
     def permitted_attributes
-      nested = @nested_attrs.map { |key, val| { key => val } }
-      @safe_attrs + nested
+      nested = _nested_attrs.map { |key, val| { key => val } }
+      _safe_attrs + nested
     end
   end
+end
+
+# Automatically include SafeParams in ActiveRecord models
+ActiveSupport.on_load(:active_record) do
+  include SafeParams
 end
